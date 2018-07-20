0f1f942c0ac9a5f8159c8c5a06ee389c016dc533
13619052620 <1959484694@qq.com>
2018-07-20 15:02:44 +0800

    测试版本

---
+++ src/main/java/com/pojo/User.java
@@ -0,0 +1,31 @@
+package com.pojo;
+
+public class User {
+    private String id;
+    private String userName;
+    private String UserPassword;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return UserPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        UserPassword = userPassword;
+    }
+}
